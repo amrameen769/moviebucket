c8b5346d5f6ca57543e70859327488da1864f6c2
Al Ameen A R <40757425+amrameen769@users.noreply.github.com>
2019-12-17 10:55:11 +0530

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,7 +1,7 @@
 <?php
-  $dbname = "db_moviebucket";
-  $server = "127.0.0.1";
-  $username = "amrameen769";
-  $password = "7025";
+  $dbname = "jw7EiqcSVy";
+  $server = "remotemysql.com";
+  $username = "jw7EiqcSVy";
+  $password = "b67MLToyew";
   $dbconn = mysqli_connect($server,$username,$password,$dbname) or die("Couldn't Connect to Database");
   $exec = new mysqli($server,$username,$password,$dbname);
